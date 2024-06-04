4558c275562e3a4730956bb153a891cee760abad
VladaChernoray <Vlada2002Vlada2001@gmail.com>
2024-06-04 22:25:56 +0300

    fix issues

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ const token = "7083409028:AAGNh5hHswJP2Trs8UbgzTslTq7-8O6pf3U";
 
 const TelegramBot = require('node-telegram-bot-api');
 const express = require('express');
-const cors = require('cors');  
+const cors = require('cors');
 
 const webAppUrl = 'https://main--test-app-frontend.netlify.app';
 
@@ -22,14 +22,15 @@ bot.on('message', async (msg) => {
   const text = msg.text;
 
   if (text === '/start') {
-    await bot.sendMessage(chatId, "Fill the form:", {
+    await bot.sendMessage(chatId, "Choose option:", {
       reply_markup: {
         inline_keyboard: [
-          [{ text: "Form", web_app: { url: webAppUrl + '/product_list' } }]
+          [{ text: "Cart", web_app: { url: webAppUrl + '/product_list' } }],
+          [{ text: "Form", web_app: { url: webAppUrl + '/form' } }]
         ],
-      }
+      },
     });
-  } 
+  }
   if (msg?.web_app_data?.data) {
     try {
       const data = JSON.parse(msg?.web_app_data?.data);
@@ -44,23 +45,23 @@ bot.on('message', async (msg) => {
       console.log(e);
     }
   }
-  // bot.sendMessage(chatId, 'Received your message'); 
+  // bot.sendMessage(chatId, 'Received your message');
 });
 
 app.post('/web-data', async (req, res) => {
-  const { queryId, products, totalPrice } = req.body;
+  const { queryID, products, totalPrice } = req.body;
   try {
-    await bot.answerWebAppQuery(queryId, {
+    await bot.answerWebAppQuery(queryID, {
       type: 'article',
-      id: queryId,
+      id: queryID,
       title: "Successful purchase",
       input_message_content: { message_text: "You purchase goods for the amount: " + totalPrice }
     });
     res.status(200).send('Query answered successfully');
   } catch (e) {
-    await bot.answerWebAppQuery(queryId, {
+    await bot.answerWebAppQuery(queryID, {
       type: 'article',
-      id: queryId,
+      id: queryID,
       title: "Failed to purchase item",
       input_message_content: { message_text: "Failed to purchase item" }
     });
@@ -68,5 +69,5 @@ app.post('/web-data', async (req, res) => {
   }
 });
 
-const PORT = 8000;
-app.listen(PORT, () => console.log('Server started on PORT ' + PORT));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server started on PORT ${PORT}`));
