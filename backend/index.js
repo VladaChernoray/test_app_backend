a0dc3dd808ec578753b834203a5e7443a0f08bbc
VladaChernoray <Vlada2002Vlada2001@gmail.com>
2024-06-04 07:22:36 +0300

    initial commit

---
+++ backend/index.js
@@ -0,0 +1,72 @@
+const token = "7083409028:AAGNh5hHswJP2Trs8UbgzTslTq7-8O6pf3U";
+
+const TelegramBot = require('node-telegram-bot-api');
+const express = require('express');
+const cors = require('cors');  
+
+const webAppUrl = 'https://main--test-app-frontend.netlify.app';
+
+const bot = new TelegramBot(token, { polling: true });
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+bot.onText(/\/echo (.+)/, (msg, match) => {
+  const chatId = msg.chat.id;
+  const resp = match[1]; 
+  bot.sendMessage(chatId, resp);
+});
+
+bot.on('message', async (msg) => {
+  const chatId = msg.chat.id;
+  const text = msg.text;
+
+  if (text === '/start') {
+    await bot.sendMessage(chatId, "Fill the form:", {
+      reply_markup: {
+        inline_keyboard: [
+          [{ text: "Form", web_app: { url: webAppUrl + '/product_list' } }]
+        ],
+      }
+    });
+  } 
+  if (msg?.web_app_data?.data) {
+    try {
+      const data = JSON.parse(msg?.web_app_data?.data);
+      await bot.sendMessage(chatId, "Thanks for the feedback");
+      await bot.sendMessage(chatId, "Your country is: " + data?.country);
+      await bot.sendMessage(chatId, "Your street is: " + data?.street);
+
+      setTimeout(async () => {
+        await bot.sendMessage(chatId, "All info you receive in chat.");  
+      }, 3000);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+  // bot.sendMessage(chatId, 'Received your message'); 
+});
+
+app.post('/web-data', async (req, res) => {
+  const { queryId, products, totalPrice } = req.body;
+  try {
+    await bot.answerWebAppQuery(queryId, {
+      type: 'article',
+      id: queryId,
+      title: "Successful purchase",
+      input_message_content: { message_text: "You purchase goods for the amount: " + totalPrice }
+    });
+    res.status(200).send('Query answered successfully');
+  } catch (e) {
+    await bot.answerWebAppQuery(queryId, {
+      type: 'article',
+      id: queryId,
+      title: "Failed to purchase item",
+      input_message_content: { message_text: "Failed to purchase item" }
+    });
+    res.status(500).send('Failed to answer query');
+  }
+});
+
+const PORT = 8000;
+app.listen(PORT, () => console.log('Server started on PORT ' + PORT));
