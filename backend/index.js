752ca405d2d228aed9dc48a87c247dd5d76e8a46
VladaChernoray <Vlada2002Vlada2001@gmail.com>
2024-06-06 00:35:16 +0300

    fix

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,4 @@
-const token = "7083409028:AAGNh5hHswJP2Trs8UbgzTslTq7-8O6pf3U";
-
+const token = process.env.TELEGRAM_BOT_TOKEN;
 const TelegramBot = require('node-telegram-bot-api');
 const express = require('express');
 const cors = require('cors');
@@ -9,7 +8,9 @@ const webAppUrl = 'https://main--test-app-frontend.netlify.app';
 const bot = new TelegramBot(token, { polling: true });
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: 'https://main--test-app-frontend.netlify.app'
+}));
 
 bot.onText(/\/echo (.+)/, (msg, match) => {
   const chatId = msg.chat.id;
@@ -45,34 +46,29 @@ bot.on('message', async (msg) => {
       console.log(e);
     }
   }
-  // bot.sendMessage(chatId, 'Received your message');
 });
 
 app.post('/web-data', async (req, res) => {
   const { queryID, products, totalPrice } = req.body;
   try {
-      await bot.answerWebAppQuery(queryID, {
-          type: 'article',
-          id: queryID,
-          title: "Successful purchase",
-          input_message_content: { message_text: "You purchase goods for the amount: " + totalPrice }
-      });
-      res.status(200).send('Query answered successfully');
+    await bot.answerWebAppQuery(queryID, {
+      type: 'article',
+      id: queryID,
+      title: "Successful purchase",
+      input_message_content: { message_text: "You purchase goods for the amount: " + totalPrice }
+    });
+    res.status(200).send('Query answered successfully');
   } catch (e) {
-      await bot.answerWebAppQuery(queryID, {
-          type: 'article',
-          id: queryID,
-          title: "Failed to purchase item",
-          input_message_content: { message_text: "Failed to purchase item" }
-      });
-      res.status(500).send('Failed to answer query');
+    console.error('Error answering query:', e);
+    await bot.answerWebAppQuery(queryID, {
+      type: 'article',
+      id: queryID,
+      title: "Failed to purchase item",
+      input_message_content: { message_text: "Failed to purchase item" }
+    });
+    res.status(500).send('Failed to answer query');
   }
 });
 
-
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server started on PORT ${PORT}`));
-
-app.use(cors({
-  origin: 'https://test-app-frontend.netlify.app'
-}));
